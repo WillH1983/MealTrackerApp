2d40231b90effc8a68d7780194104b416c6452e8
Will Hindenburg <WillH1983@yahoo.com>
2012-07-21 18:39:59 -0500

    Initial Commit

---
+++ MealTracker/MealTrackerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MealTrackerAppDelegate.h
+//  MealTracker
+//
+//  Created by Will Hindenburg on 7/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MealTrackerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MealTracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MealTracker
+//
+//  Created by Will Hindenburg on 7/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MealTrackerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MealTrackerAppDelegate class]));
+    }
+}
